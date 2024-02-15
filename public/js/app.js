5645c68a6bf7266b7faee8053e61a5a1323e96fa
lucasjotap <leiathemosthot@gmail.com>
2024-02-15 13:01:30 -0300

    newest changes with map

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ class ProductList extends React.Component {
     return Math.floor((Math.random() * 50) + 15);
   }
 
-  static x =     {
+  static products =  [{
       id: 1,
       title: 'Yellow Pail',
       description: 'On-demand sand castle construction expertise.',
@@ -16,13 +16,39 @@ class ProductList extends React.Component {
       votes: ProductList.generateVoteCount(),
       submitterAvatarUrl: 'images/avatars/daniel.jpg',
       productImageUrl: 'images/products/image-aqua.png',
-    }
+    },
+    {
+      id: 2,
+      title: 'Supermajority: The Fantasy Congress League',
+      description: 'Earn points when your favorite politicians pass legislation.',
+      url: '#',
+      votes: ProductList.generateVoteCount(),
+      submitterAvatarUrl: 'images/avatars/kristy.png',
+      productImageUrl: 'images/products/image-rose.png',
+    },
+    {
+      id: 3,
+      title: 'Tinfoild: Tailored tinfoil hats',
+      description: 'We already have your measurements and shipping address.',
+      url: '#',
+      votes: ProductList.generateVoteCount(),
+      submitterAvatarUrl: 'images/avatars/veronika.jpg',
+      productImageUrl: 'images/products/image-steel.png',
+    },
+    {
+      id: 4,
+      title: 'Haught or Naught',
+      description: 'High-minded or absent-minded? You decide.',
+      url: '#',
+      votes: ProductList.generateVoteCount(),
+      submitterAvatarUrl: 'images/avatars/molly.png',
+      productImageUrl: 'images/products/image-yellow.png',
+    }]
 
   render(){
-    const product = ProductList.x;
-    return(
-      <div className='ui unstackable items'>
-        <Product 
+    const productComponents = ProductList.products.map((product) => (
+        <Product
+        key={'product-' + product.id} 
         id={product.id}
         title={product.title}
         description={product.description}
@@ -31,6 +57,10 @@ class ProductList extends React.Component {
         submitterAvatarUrl={product.submitterAvatarUrl}
         productImageUrl={product.productImageUrl}
         /> 
+    ));
+    return (
+      <div className='ui unstackable items'>
+        {productComponents}
       </div>
     );
   }
