7f39646ff95ade280d1f31c1ae5d471c9e7c7158
lucasjotap <leiathemosthot@gmail.com>
2024-02-19 12:18:24 -0300

    fixed seed.js bug

--- public/js/app.js
+++ public/js/app.js
@@ -4,57 +4,28 @@
 
 class ProductList extends React.Component {
 
-  static generateVoteCount() {
-    return Math.floor((Math.random() * 50) + 15);
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ products: Seed.products });
   }
 
   handleProductUpVote(productId) {
     console.log(productId + ' was updated.');
   }
 
-  static products =  [{
-      id: 1,
-      title: 'Yellow Pail',
-      description: 'On-demand sand castle construction expertise.',
-      url: '#',
-      votes: ProductList.generateVoteCount(),
-      submitterAvatarUrl: 'images/avatars/daniel.jpg',
-      productImageUrl: 'images/products/image-aqua.png',
-    },
-    {
-      id: 2,
-      title: 'Supermajority: The Fantasy Congress League',
-      description: 'Earn points when your favorite politicians pass legislation.',
-      url: '#',
-      votes: ProductList.generateVoteCount(),
-      submitterAvatarUrl: 'images/avatars/kristy.png',
-      productImageUrl: 'images/products/image-rose.png',
-    },
-    {
-      id: 3,
-      title: 'Tinfoild: Tailored tinfoil hats',
-      description: 'We already have your measurements and shipping address.',
-      url: '#',
-      votes: ProductList.generateVoteCount(),
-      submitterAvatarUrl: 'images/avatars/veronika.jpg',
-      productImageUrl: 'images/products/image-steel.png',
-    },
-    {
-      id: 4,
-      title: 'Haught or Naught',
-      description: 'High-minded or absent-minded? You decide.',
-      url: '#',
-      votes: ProductList.generateVoteCount(),
-      submitterAvatarUrl: 'images/avatars/molly.png',
-      productImageUrl: 'images/products/image-yellow.png',
-    }]
-
-  render(){
-    const products = ProductList.products.sort((a, b) =>(
+  render() {
+    const products = this.state.products.sort((a, b) => (
       b.votes - a.votes
-      ));
+    ));
     
-    const productComponents = ProductList.products.map((product) => (
+    const productComponents = products.map((product) => (
         <Product
         key={'product-' + product.id} 
         id={product.id}
